32b5eff1e5f25ac9a493305a50d36ff90cd41fb3
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-21 20:32:52 +0900

    todo編集機能を追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    {{ msg }}
     <form>
-      <button>ADD TASK</button>
-      <button>DELETE FINISHED TASKS</button>
-      <p>input: <input type="text"></p>
-      <p>task:</p>
+      <button @click="addTodo()">ADD TASK</button>
+      <button @click="removeTodo()">DELETE FINISHED TASKS</button>
+      <p>input: <input type="text" v-model="newTodo"></p>
+      <p>task: {{ newTodo }}</p>
     </form>
     <div class="task-list">
-    <label class="tasl-list__item"
-           v-for="todo in todos">
-           <input type="checkbox"><button>EDIT</button>{{ todo.text }}
-     </label>
-      
+      <label class="task-list__item"
+             v-for="todo in todos">
+        <input type="checkbox" v-model="todo.done">
+        <input type="checkbox" v-model="todo.editing">
+        <input v-if="todo.editing" v-model="todo.text" @keyup.enter="todo.editing = !todo.editing">
+        <span v-else>{{ todo.text }}</span>
+      </label>
     </div>
   </div>
 </template>
@@ -20,15 +21,35 @@
 <script>
 export default {
   name: 'hello',
-  data () {
+  data: function () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
       todos : [
-        {text : 'vue-router', done: false},
-        {text : 'vuex', done: false},
-        {text : 'vue-loader', done: false},
-        {text : 'awesome-vue', done: true },
-      ]
+        {text : 'vue-router', done: false, editing: false},
+        {text : 'vuex', done: false, editing: false},
+        {text : 'vue-loader', done: false, editing: false},
+        {text : 'awesome-vue', done: true, editing: false},
+      ],
+      newTodo: ""
+    }
+  },
+  methods: {
+    addTodo: function(event) {
+      let text = this.newTodo && this.newTodo.trim()
+      if (!text) {
+        return
+      }
+      this.todos.push({
+        text: text,
+        done: false,
+        editing: false
+      })
+      this.newTodo = ''
+    },
+    removeTodo: function (event) {
+      for (let i = this.todos.length - 1; i >= 0; i--) {
+        if (this.todos[i].done) this.todos.splice(i, 1)
+      }
     }
   }
 }
